797ef09c23e8f057cfb0fef34886f8ae6bb63fc0
Dhirendra Singh <dhi.singh@gmail.com>
2017-03-24 10:32:40 +1100

    bushfire: default to jill

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,12 +10,12 @@
 # it under the terms of the GNU Lesser General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Lesser Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Lesser Public
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -23,15 +23,15 @@
 ###
 
 DIR=`dirname "$0"`
-PROGRAM='java -cp bushfire-1.0.1-SNAPSHOT.jar io.github.agentsoz.bushfire.BushfireMain'
+PROGRAM="java -cp bushfire-1.0.1-SNAPSHOT.jar io.github.agentsoz.bushfire.matsimjill.Main"
 #DEFAULT_ARGS='-c scenarios/halls_gap/halls_gap.xml -l halls-gap.log -level INFO'
-DEFAULT_ARGS='-c scenarios/maldon/maldon.xml -l maldon.log -level INFO'
+DEFAULT_ARGS="--config scenarios/maldon-simple/maldon.xml --logfile maldon.log --loglevel INFO --jillconfig '--config={agents:[{classname:io.github.agentsoz.bushfire.matsimjill.agents.Resident, args:null, count:700}],logLevel: WARN,logFile: Main.log,programOutputFile: Main.out}'"
 
-# java -cp bushfire-1.0.1-SNAPSHOT.jar io.github.agentsoz.bushfire.matsimjill.Main --config scenarios/maldon-simple/maldon.xml --logfile maldon.log --loglevel INFO --jillconfig "--config={agents:[{classname:io.github.agentsoz.bushfire.matsimjill.agents.Resident, args:null, count:700}],logLevel: WARN,logFile: Main.log,programOutputFile: Main.out}" 
+# java -cp bushfire-1.0.1-SNAPSHOT.jar io.github.agentsoz.bushfire.matsimjill.Main --config scenarios/maldon-simple/maldon.xml --logfile maldon.log --loglevel INFO --jillconfig \\"--config={agents:[{classname:io.github.agentsoz.bushfire.matsimjill.agents.Resident, args:null, count:700}],logLevel: WARN,logFile: Main.log,programOutputFile: Main.out}\\"
 
 
 # Print usage
-$PROGRAM -h
+$PROGRAM --help
 
 # print args in use
 printf "default args:\n  $DEFAULT_ARGS\n\n"
@@ -49,7 +49,8 @@ CMD="$PROGRAM $DEFAULT_ARGS $USER_ARGS"
 printf "running:\n  "
 printf "started on `date +"%B %d, %Y at %r"` \n  "
 printf "$CMD\n  "
-$CMD >/dev/null 2>&1
+#$CMD >/dev/null 2>&1
+eval $CMD
 printf "finished on `date +"%B %d, %Y at %r"` \n\n"
 
 # To run the Maldon scenario with Jill agents, do:
