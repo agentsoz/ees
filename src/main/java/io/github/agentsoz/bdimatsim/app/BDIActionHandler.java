98f8ce498f090b31f4e347abf0a27bf9e36fd511
kainagel <nagel@vsp.tu-berlin.de>
2017-11-05 20:00:14 +1100

    a comment

--- src/main/java/io/github/agentsoz/bdimatsim/app/BDIActionHandler.java
+++ src/main/java/io/github/agentsoz/bdimatsim/app/BDIActionHandler.java
@@ -27,5 +27,10 @@ import io.github.agentsoz.bdimatsim.MATSimModel;
 public interface BDIActionHandler {
 	
 	public boolean handle(String agentID, String actionID, Object[] actionArgs, MATSimModel model); 
+	// yyyy we have a tendency to pass "global" infrastructure (here: MATSim Model) rather through the constructor.  Reasons:
+	// * The constructor is not part of the interface, so it is easier to change in specific implementations.
+	// * People start putting something like "this.model = model" into the handle method in order to remember the global
+	// object elsewhere in the handler class.  Then, however, it is more transparent to have it in the constructor right away.
+	// kai, nov'17
 
 }
