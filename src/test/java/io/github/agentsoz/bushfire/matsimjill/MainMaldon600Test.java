1de5d7e94926247d22b5d19b0d8fb64231420583
kainagel <nagel@vsp.tu-berlin.de>
2017-11-11 14:25:08 +1100

    pull tests into different classes; some other changes

--- src/test/java/io/github/agentsoz/bushfire/matsimjill/MainMaldon600Test.java
+++ src/test/java/io/github/agentsoz/bushfire/matsimjill/MainMaldon600Test.java
@@ -14,49 +14,11 @@ import io.github.agentsoz.bushfire.matsimjill.Main;
  * @author dsingh
  *
  */
-public class MainTest {
+public class MainMaldon600Test {
+	// have tests in separate classes so that they run, at least under maven, in separate JVMs.  kai, nov'17
 
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
-	@Test
-	public void testCampbellsCreek50() {
-
-	        String [] args = {
-	                "--config",  "scenarios/campbells-creek/scenario_main.xml", 
-	                "--logfile", "scenarios/campbells-creek/scenario.log",
-	                "--loglevel", "INFO",
-//	                "--plan-selection-policy", "FIRST", // ensures it is deterministic, as default is RANDOM
-	                "--seed", "12345",
-	                "--jillconfig", "--config={"+
-	                        "agents:[{classname:io.github.agentsoz.bushfire.matsimjill.agents.Resident, args:null, count:50}],"+
-	                        "logLevel: WARN,"+
-	                        "logFile: \"scenarios/campbells-creek/jill.log\","+
-	                        "programOutputFile: \"scenarios/campbells-creek/jill.out\","+
-	                        "randomSeed: 12345,"+ // jill random seed
-	                        "numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
-	        "}"};
-
-	        Main.main(args);
-
-	        {
-	            long expectedCRC = CRCChecksum.getCRCFromFile( utils.getInputDirectory() + "/output_plans.xml.gz" ) ;
-	            long actualCRC = CRCChecksum.getCRCFromFile( "output/output_plans.xml.gz" ) ; // 2704415442
-	            System.err.println( "plans: expected=" + expectedCRC + "; actual=" + actualCRC ) ;
-	            Assert.assertEquals (expectedCRC, actualCRC);
-	        }
-	        {
-	            long expectedCRC = CRCChecksum.getCRCFromFile( utils.getInputDirectory() + "/output_events.xml.gz" ) ;
-	            long actualCRC = CRCChecksum.getCRCFromFile( "output/output_events.xml.gz" ) ; // 1380811447
-	            System.err.println( "plans: expected=" + expectedCRC + "; actual=" + actualCRC ) ;
-	            Assert.assertEquals (expectedCRC, actualCRC);
-	        }
-
-	        {
-	            long expectedCRC = CRCChecksum.getCRCFromFile( utils.getInputDirectory() + "/jill.out" ) ;
-	            long actualCRC = CRCChecksum.getCRCFromFile( "scenarios/campbells-creek/jill.out" ) ;
-	            Assert.assertEquals (expectedCRC, actualCRC); 
-	        }
-	    }
 
 	@SuppressWarnings("static-method")
 	@Test
