c9b705359de08f8b7205105ca540ca0c9a3c7c0a
Dhirendra Singh <dhi.singh@gmail.com>
2017-11-22 13:27:29 +1100

    Adding in delay of 5 secs between evac signal and dirve_to action

--- src/main/java/io/github/agentsoz/bdimatsim/DRIVETODefaultActionHandler.java
+++ src/main/java/io/github/agentsoz/bdimatsim/DRIVETODefaultActionHandler.java
@@ -65,7 +65,61 @@ public final class DRIVETODefaultActionHandler implements BDIActionHandler {
 			throw new RuntimeException("Destination coordinates are not given");
 		}
 
-		DRIVETODefaultActionHandler.attachNewActivityAtEndOfPlan(newLinkId, Id.createPersonId(agentID), model);
+		double departureTime = (double)args[2];
+		
+		//double now = model.getTime() ; 
+    
+    		MobsimAgent agent1 = model.getMobsimDataProvider().getAgents().get(Id.createPersonId(agentID));
+    
+    		Plan plan = WithinDayAgentUtils.getModifiablePlan(agent1) ;
+    
+    		List<PlanElement> planElements = plan.getPlanElements() ;
+    
+    		int planElementsIndex = planElements.size()-1;
+    		// seems that under normal circumstances in this pgm, size returns 1 und idx is thus 0.
+    
+    		Activity lastAct = (Activity)planElements.get(planElementsIndex);
+    		// i.e. this would be the first activity
+    
+    		double endTime = departureTime;
+    		if(endTime <= lastAct.getStartTime() +10){
+    			endTime = lastAct.getStartTime() +10;
+    		}
+    		lastAct.setEndTime(endTime);
+    
+    		// now the real work begins. This changes the activity (i.e. the destination of the current leg) and then
+    		// re-splices the plan
+    
+    		Activity newAct = model.getScenario().getPopulation().getFactory().createActivityFromLinkId("work", newLinkId ) ;
+    
+    		Leg newLeg = model.getScenario().getPopulation().getFactory().createLeg(TransportMode.car);
+    //		// new Route for current Leg.
+    		newLeg.setDepartureTime(endTime);
+    //		editRoutes.relocateFutureLegRoute(newLeg, lastAct.getLinkId(), newActivityLinkId,((HasPerson)agent).getPerson());
+    		// --- old version end
+    
+    		newAct.setEndTime( Double.POSITIVE_INFINITY ) ;
+    
+    		planElements.add(newLeg);
+    		planElements.add(newAct); 
+    		
+    //		final List<Trip> trips = TripStructureUtils.getTrips(planElements, stageActivities);
+    		
+    //		Gbl.assertIf( trips.size()>=1 );
+    		
+    		List<PlanElement> sublist = planElements.subList(planElements.size()-3, planElements.size() ) ;
+    		// (the sub-list is "exclusive" the right index)
+    		
+    		Trip trip = TripStructureUtils.getTrips(sublist, model.getReplanner().getEditTrips().getStageActivities()).get(0) ;
+    		model.getReplanner().getEditTrips().replanFutureTrip(trip, plan, TransportMode.car) ;
+    
+    		WithinDayAgentUtils.resetCaches(agent1);
+    		// this is necessary since the simulation may have cached some info from the plan at other places.
+    		// (May not be necessary in this particular situation since there is nothing to cache when an agent is at an activity.) kai, feb'14
+    
+    		model.getReplanner().getEditPlans().rescheduleActivityEnd(agent1);
+    		// this is the only place where the internal interface is truly needed, since it is the only place where the agent needs to be "woken up".
+    		// This is necessary since otherwise the simulation will not touch the agent until its previously scheduled activity end. kai, feb/14
 
 		// Now register a event handler for when the agent arrives at the destination
 		PAAgent agent = model.getAgentManager().getAgent( agentID );
@@ -84,112 +138,4 @@ public final class DRIVETODefaultActionHandler implements BDIActionHandler {
 		);
 		return true;
 	}
-
-	protected static final void attachNewActivityAtEndOfPlan(Id<Link> newActivityLinkId, Id<Person> agentId, MATSimModel model ) {
-			// called at least once
-	
-			// yyyy if the current activity is not already the last activity of the agent, this method may not behave as expected. kai, feb'14
-	
-			//changing the end time of the activity can be replaced by the forceEndActivity method
-	
-			double now = model.getTime() ; 
-	
-			MobsimAgent agent = model.getMobsimDataProvider().getAgents().get(agentId);
-	
-			Plan plan = WithinDayAgentUtils.getModifiablePlan(agent) ;
-	
-			List<PlanElement> planElements = plan.getPlanElements() ;
-	
-			int planElementsIndex = planElements.size()-1;
-			// seems that under normal circumstances in this pgm, size returns 1 und idx is thus 0.
-	
-			Activity lastAct = (Activity)planElements.get(planElementsIndex);
-			// i.e. this would be the first activity
-	
-			double endTime = now;
-			if(endTime <= lastAct.getStartTime() +10){
-				endTime = lastAct.getStartTime() +10;
-			}
-			lastAct.setEndTime(endTime);
-	
-			// now the real work begins. This changes the activity (i.e. the destination of the current leg) and then
-			// re-splices the plan
-	
-			Activity newAct = model.getScenario().getPopulation().getFactory().createActivityFromLinkId("work", newActivityLinkId ) ;
-	
-			Leg newLeg = model.getScenario().getPopulation().getFactory().createLeg(TransportMode.car);
-	//		// new Route for current Leg.
-			newLeg.setDepartureTime(endTime);
-	//		editRoutes.relocateFutureLegRoute(newLeg, lastAct.getLinkId(), newActivityLinkId,((HasPerson)agent).getPerson());
-			// --- old version end
-	
-			newAct.setEndTime( Double.POSITIVE_INFINITY ) ;
-	
-			planElements.add(newLeg);
-			planElements.add(newAct); 
-			
-	//		final List<Trip> trips = TripStructureUtils.getTrips(planElements, stageActivities);
-			
-	//		Gbl.assertIf( trips.size()>=1 );
-			
-			List<PlanElement> sublist = planElements.subList(planElements.size()-3, planElements.size() ) ;
-			// (the sub-list is "exclusive" the right index)
-			
-			Trip trip = TripStructureUtils.getTrips(sublist, model.getReplanner().getEditTrips().getStageActivities()).get(0) ;
-			model.getReplanner().getEditTrips().replanFutureTrip(trip, plan, TransportMode.car) ;
-	
-			WithinDayAgentUtils.resetCaches(agent);
-			// this is necessary since the simulation may have cached some info from the plan at other places.
-			// (May not be necessary in this particular situation since there is nothing to cache when an agent is at an activity.) kai, feb'14
-	
-			model.getReplanner().getEditPlans().rescheduleActivityEnd(agent);
-			// this is the only place where the internal interface is truly needed, since it is the only place where the agent needs to be "woken up".
-			// This is necessary since otherwise the simulation will not touch the agent until its previously scheduled activity end. kai, feb/14
-	
-		}
-		/*
-		protected final boolean removeActivities(Id<Person> agentId) {
-			// possibly never called
-	
-			Map<Id<Person>, MobsimAgent> mapping = model.getMobsimAgentMap();
-			MobsimAgent agent = mapping.get(agentId);
-	
-			Plan plan = WithinDayAgentUtils.getModifiablePlan(agent) ;
-	
-			List<PlanElement> planElements = plan.getPlanElements() ;
-	
-			while(planElements.size() > 1){
-				planElements.remove(planElements.size()-1);
-			}
-	
-			Activity act = (Activity) planElements.get(0);
-			act.setEndTime(3600*24);
-			act.setMaximumDuration(3600*24);
-	
-			WithinDayAgentUtils.resetCaches(agent);
-			this.qsim.rescheduleActivityEnd(agent);
-			return true;
-		}
-		 */
-		/*
-		protected final boolean changeActivityEndTimeByPlanElementIndex(Id<Person> agentId, int planElementIndex, double newEndTime) {
-			// probably never called
-	
-			Map<Id<Person>, MobsimAgent> mapping = model.getMobsimAgentMap();
-			MobsimAgent agent = mapping.get(agentId);
-	
-			Plan plan = WithinDayAgentUtils.getModifiablePlan(agent) ;
-	
-			List<PlanElement> planElements = plan.getPlanElements() ;
-	
-	
-			Activity act = (Activity) planElements.get(planElementIndex);
-			act.setEndTime(newEndTime);
-			act.setMaximumDuration(3600*24);
-	
-			WithinDayAgentUtils.resetCaches(agent);
-			this.qsim.rescheduleActivityEnd(agent);
-			return true;
-		}
-		 */
 }
