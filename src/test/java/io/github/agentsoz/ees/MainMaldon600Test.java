337e450a1699c7fd2bd084ea2a00886e4c247d31
Dhirendra Singh <dhi.singh@gmail.com>
2018-01-17 09:05:45 +1100

    Started consolidating duplicated xsd and matsim network files in scenarios

--- src/test/java/io/github/agentsoz/ees/MainMaldon600Test.java
+++ src/test/java/io/github/agentsoz/ees/MainMaldon600Test.java
@@ -36,18 +36,18 @@ public class MainMaldon600Test {
 --jillconfig "--config={agents:[{classname:Resident, args:null, count:600}],logLevel: WARN,logFile: \"/var/www/data/user-data/2017-10-23-ds-maldon/scenario/jill.log\",programOutputFile: \"/var/www/data/user-data/2017-10-23-ds-maldon/scenario/jill.out\"}"
 		 */
 		String [] args = {
-				"--config",  "scenarios/maldon-2017-11-01/scenario_main.xml",
-				"--logfile", "scenarios/maldon-2017-11-01/scenario.log",
+				"--config",  "scenarios/mount-alexander-shire/maldon600/scenario_main.xml",
+				"--logfile", "scenarios/mount-alexander-shire/maldon600/scenario.log",
 				"--loglevel", "INFO",
 				//				"--plan-selection-policy", "FIRST", // ensures it is deterministic, as default is RANDOM
 				"--seed", "12345",
-				"--safeline-output-file-pattern", "scenarios/campbells-creek/safeline.%d%.out",
+				"--safeline-output-file-pattern", "scenarios/mount-alexander-shire/maldon600/safeline.%d%.out",
 				"--matsim-output-directory", utils.getOutputDirectory(),
 				"--jillconfig", "--config={"+
 						"agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:600}],"+
 						"logLevel: WARN,"+
-						"logFile: \"scenarios/maldon-2017-11-01/jill.log\","+
-						"programOutputFile: \"scenarios/maldon-2017-11-01/jill.out\","+
+						"logFile: \"scenarios/mount-alexander-shire/maldon600/jill.log\","+
+						"programOutputFile: \"scenarios/mount-alexander-shire/maldon600/jill.out\","+
 						"randomSeed: 12345"+ // jill random seed
 						//"numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
 						"}"
