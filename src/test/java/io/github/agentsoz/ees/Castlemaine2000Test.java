ffd822d6d73f9b671be707621afd0e0f78aafbf4
Dhirendra Singh <dhi.singh@gmail.com>
2018-02-01 12:39:25 +1100

    Resident agents now react to congestion in the same way as for blockages; beginnings of a test in Castlemaine2000Test; towards #8

---
+++ src/test/java/io/github/agentsoz/ees/Castlemaine2000Test.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package io.github.agentsoz.ees;
+
+import org.apache.log4j.Logger;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+/**
+ * @author dsingh
+ *
+ */
+public class Castlemaine2000Test {
+	// have tests in separate classes so that they run, at least under maven, in separate JVMs.  kai, nov'17
+	
+	private static final Logger log = Logger.getLogger(Castlemaine2000Test.class) ;
+
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+
+	@SuppressWarnings("static-method")
+	@Test
+	@Ignore
+	public void testCastlemaine2000() {
+		String [] args = {
+				"--config",  "scenarios/mount-alexander-shire/castlemaine-2000/scenario_main.xml",
+				"--logfile", "scenarios/mount-alexander-shire/castlemaine-2000/scenario.log",
+				"--loglevel", "INFO",
+				//				"--plan-selection-policy", "FIRST", // ensures it is deterministic, as default is RANDOM
+				"--seed", "12345",
+				"--safeline-output-file-pattern", "scenarios/mount-alexander-shire/castlemaine-2000/safeline.%d%.out",
+				"--matsim-output-directory", utils.getOutputDirectory(),
+				"--jillconfig", "--config={"+
+						"agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:2000}],"+
+						"logLevel: WARN,"+
+						"logFile: \"scenarios/mount-alexander-shire/castlemaine-2000/jill.log\","+
+						"programOutputFile: \"scenarios/mount-alexander-shire/castlemaine-2000/jill.out\","+
+						"randomSeed: 12345"+ // jill random seed
+						//"numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
+						"}"
+		};
+
+		Main.main(args);
+
+		// FIXME: enable the checks below when expected results are verified and saved; DS 23/jan/18
+		/*
+		final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
+		long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
+		log.warn("actual(events)="+actualEventsCRC) ;
+		long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
+		log.warn("actual(plans)="+actualPlansCRC) ;
+		
+		final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
+		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 1.);
+		TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, true);
+
+		
+		long [] expectedPlans = new long [] {
+				CRCChecksum.getCRCFromFile( utils.getInputDirectory() + "/output_plans.xml.gz" )
+		};
+		TestUtils.checkSeveral(expectedPlans, actualPlansCRC);
+		*/
+	}
+}
