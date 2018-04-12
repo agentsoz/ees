5d2d6c9d0583097f336c5a13231e97de943aafdd
osbornejr <joelwilliamrobertson@gmail.com>
2018-04-12 13:52:26 +1000

    Added checks to both Otways300 tests

--- src/test/java/io/github/agentsoz/ees/Otways300WithTouristsTestOLD.java
+++ src/test/java/io/github/agentsoz/ees/Otways300WithTouristsTestOLD.java
@@ -3,9 +3,12 @@
  */
 package io.github.agentsoz.ees;
 
+import io.github.agentsoz.bdimatsim.EvacConfig;
+import io.github.agentsoz.util.TestUtils;
 import org.apache.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.core.utils.misc.CRCChecksum;
 import org.matsim.testcases.MatsimTestUtils;
 
 /**
@@ -16,7 +19,7 @@ public class Otways300WithTouristsTestOLD {
 
     // have tests in separate classes so that they run, at least under maven, in separate JVMs.  kai, nov'17
 
-    private static final Logger log = Logger.getLogger(Otways300WithTouristsTestOLD.class);
+    private static final Logger log = Logger.getLogger(Otways300WithTouristsTest.class);
 
     @Rule
     public MatsimTestUtils utils = new MatsimTestUtils();
@@ -34,34 +37,34 @@ public class Otways300WithTouristsTestOLD {
                 "--matsim-output-directory", utils.getOutputDirectory(),
                 "--jillconfig", "--config={" +
                 "agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:290}," +
-                    "{classname:io.github.agentsoz.ees.agents.Tourist, " +
-                    "args:[--WayHome,\"MelbourneRoute,787484,5764290\",--CongestionBehaviour,1200,0.5], count:10}]," +
+                "{classname:io.github.agentsoz.ees.agents.Tourist, " +
+                "args:[--WayHome,\"MelbourneRoute,787484,5764290\",--CongestionBehaviour,1200,0.5], count:10}]," +
                 "logLevel: WARN," +
                 "logFile: \"scenarios/surf-coast-shire/otways-300-old/jill.log\"," +
                 "programOutputFile: \"scenarios/surf-coast-shire/otways-300-old/jill.out\"," +
-                "randomSeed: 12345" + // jill random seed
-                //"numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
+                "randomSeed: 12345," + // jill random seed
+                "numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
                 "}"};
 
         Main.main(args);
 
-//        final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
-//        long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
-//        System.err.println("actual(events)="+actualEventsCRC) ;
-//
-//        long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
-//        System.err.println("actual(plans)="+actualPlansCRC) ;
-//
-//        // ---
-//
-//        final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
-//
-//        // ---
-//
-//        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,10.);
-//        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,10.);
-//        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 10.);
-//        TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
+        final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
+        long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
+        System.err.println("actual(events)="+actualEventsCRC) ;
+
+        long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
+        System.err.println("actual(plans)="+actualPlansCRC) ;
+
+        // ---
+
+        final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
+
+        // ---
+
+        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,10.);
+        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,10.);
+        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 10.);
+        TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
     }
 }
 
