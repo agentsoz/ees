7569be881c01b33db296b6c238462fd47843032f
osbornejr <joelwilliamrobertson@gmail.com>
2018-05-18 13:03:03 +1000

    switching congestion back off for Anglesea12kTest

--- src/test/java/io/github/agentsoz/ees/Anglesea12kTest.java
+++ src/test/java/io/github/agentsoz/ees/Anglesea12kTest.java
@@ -41,7 +41,7 @@ public class Anglesea12kTest {
                 "randomSeed: 12345," + // jill random seed
                 "numThreads: 1" + // run jill in single-threaded mode so logs are deterministic
                 "}",
-                "--x-congestion-config", "1000:100", // virtually disallow congestion re-routing (painfully slow otherwise!)
+                "--x-congestion-config", "100000:100000", // virtually disallow congestion re-routing (painfully slow otherwise!)
                 "--sendFireAlertOnFireStart", "false", // disable fire alert from fire model, instead will use messaging
                 "--x-messages-file", "scenarios/surf-coast-shire/anglesea-12k/scenario_messages.json", // specifies when to send evac now msg
                 "--x-zones-file", "scenarios/surf-coast-shire/anglesea-12k/Anglesea_SA1s_WSG84.json", // map from zone (SA1) ids to shapes
