1fd49249410f9dbb75814df8508ad10e90e0db56
Dhirendra Singh <dhi.singh@gmail.com>
2018-08-06 06:51:48 +1000

    Running Castlemaine1000Test at 1% sample; added expected files; towards #51

--- src/test/java/io/github/agentsoz/ees/Castlemaine1000Test.java
+++ src/test/java/io/github/agentsoz/ees/Castlemaine1000Test.java
@@ -3,9 +3,12 @@
  */
 package io.github.agentsoz.ees;
 
+import io.github.agentsoz.util.TestUtils;
 import org.apache.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.core.utils.misc.CRCChecksum;
 import org.matsim.testcases.MatsimTestUtils;
 
 /**
@@ -22,20 +25,64 @@ public class Castlemaine1000Test {
 
 	@SuppressWarnings("static-method")
 	@Test
-	public void testCastlemaine1000() {
+	public void testCastlemaine1000Reduced() {
 		String [] args = {
-				"--config",  "scenarios/mount-alexander-shire/castlemaine-1000/scenario_main.xml",
-				"--logfile", "scenarios/mount-alexander-shire/castlemaine-1000/scenario.log",
+				"--config",  "scenarios/mount-alexander-shire/castlemaine-1000/scenario_main_reduced.xml",
+				"--logfile", utils.getOutputDirectory()+"../scenario.log",
 				"--loglevel", "INFO",
 				//				"--plan-selection-policy", "FIRST", // ensures it is deterministic, as default is RANDOM
 				"--seed", "12345",
-				"--safeline-output-file-pattern", "scenarios/mount-alexander-shire/castlemaine-1000/safeline.%d%.out",
+				"--safeline-output-file-pattern", utils.getOutputDirectory()+"../safeline.%d%.out",
 				"--matsim-output-directory", utils.getOutputDirectory(),
 				"--jillconfig", "--config={"+
-						"agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:2000}],"+
+				"agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:10}],"+
+				"logLevel: WARN,"+
+				"logFile: \""+utils.getOutputDirectory()+"../jill.log\","+
+				"programOutputFile: \""+utils.getOutputDirectory()+"../jill.out\","+
+				"randomSeed: 12345"+ // jill random seed
+				//"numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
+				"}",
+				"--x-congestion-config", "180:0.25"
+		};
+
+		Main.main(args);
+
+		final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
+		long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
+		System.err.println("actual(events)="+actualEventsCRC) ;
+
+		long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
+		System.err.println("actual(plans)="+actualPlansCRC) ;
+
+		// ---
+
+		final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
+
+		// ---
+
+		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,5.);
+		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,5.);
+		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 5.);
+		TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
+	}
+
+	@SuppressWarnings("static-method")
+	@Test
+	@Ignore
+	public void testCastlemaine1000() {
+		String [] args = {
+				"--config",  "scenarios/mount-alexander-shire/castlemaine-1000/scenario_main.xml",
+				"--logfile", utils.getOutputDirectory()+"../scenario.log",
+				"--loglevel", "INFO",
+				//				"--plan-selection-policy", "FIRST", // ensures it is deterministic, as default is RANDOM
+				"--seed", "12345",
+				"--safeline-output-file-pattern", utils.getOutputDirectory()+"../safeline.%d%.out",
+				"--matsim-output-directory", utils.getOutputDirectory(),
+				"--jillconfig", "--config={"+
+						"agents:[{classname:io.github.agentsoz.ees.agents.Resident, args:null, count:1000}],"+
 						"logLevel: WARN,"+
-						"logFile: \"scenarios/mount-alexander-shire/castlemaine-1000/jill.log\","+
-						"programOutputFile: \"scenarios/mount-alexander-shire/castlemaine-1000/jill.out\","+
+						"logFile: \""+utils.getOutputDirectory()+"../jill.log\","+
+						"programOutputFile: \""+utils.getOutputDirectory()+"../jill.out\","+
 						"randomSeed: 12345"+ // jill random seed
 						//"numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
 						"}",
