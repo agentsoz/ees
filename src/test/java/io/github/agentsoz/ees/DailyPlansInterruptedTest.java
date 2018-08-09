efb6dea0a2344a9793eec9c012db6a81056beeea
Dhirendra Singh <dhi.singh@gmail.com>
2018-08-09 16:35:48 +1000

    First test with MATSim agents mixed with BDI agents; all with daily plans; and only some BDI agents evacuating based on zoned messaging; towards #43

--- src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
+++ src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
@@ -3,10 +3,12 @@
  */
 package io.github.agentsoz.ees;
 
+import io.github.agentsoz.util.TestUtils;
 import org.apache.log4j.Logger;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.core.utils.misc.CRCChecksum;
 import org.matsim.testcases.MatsimTestUtils;
 
 /**
@@ -23,7 +25,6 @@ public class DailyPlansInterruptedTest {
     public MatsimTestUtils utils = new MatsimTestUtils();
 
     @Test
-    @Ignore
     public void testDailyPlansInterrupted() {
 
         String[] args = {
@@ -43,9 +44,29 @@ public class DailyPlansInterruptedTest {
                 "}",
                 "--x-congestion-config", "180:0.33:1.0",
                 "--x-load-bdi-agents-from-matsim-plans-file", "true", // Load agents from MATSim plans file
+                "--x-messages-file", "scenarios/surf-coast-shire/plan-interrupted/scenario_messages.json", // specifies when to send evac now msg
+                "--x-zones-file", "scenarios/surf-coast-shire/plan-interrupted/Anglesea_SA1s_WSG84.json", // map from zone (SA1) ids to shapes
         };
 
         Main.main(args);
+
+        final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
+        long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
+        System.err.println("actual(events)="+actualEventsCRC) ;
+
+        long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
+        System.err.println("actual(plans)="+actualPlansCRC) ;
+
+        // ---
+
+        final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
+
+        // ---
+
+        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,5.);
+        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,5.);
+        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 5.);
+        TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
 
     }
 
