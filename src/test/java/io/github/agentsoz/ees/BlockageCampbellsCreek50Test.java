6a0f4779cf40f96b9976a720478cb0e9e40b506f
Dhirendra Singh <dhi.singh@gmail.com>
2018-09-07 12:29:57 +1000

    Updated BlockageCampbellsCreek50Test to v2

--- src/test/java/io/github/agentsoz/ees/BlockageCampbellsCreek50Test.java
+++ src/test/java/io/github/agentsoz/ees/BlockageCampbellsCreek50Test.java
@@ -5,6 +5,7 @@ package io.github.agentsoz.ees;
 
 import io.github.agentsoz.bdimatsim.MATSimModel;
 import io.github.agentsoz.util.TestUtils;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.core.utils.misc.CRCChecksum;
@@ -24,6 +25,36 @@ public class BlockageCampbellsCreek50Test {
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
 	@Test
+	public void testBlockage50V2() {
+
+		utils.getOutputDirectory(); // creates a clean one so need to call this first
+		String[] args = {
+				"--config", "scenarios/mount-alexander-shire/campbells-creek-50/ees.xml",
+		};
+		Run.main(args);
+
+		final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
+		long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
+		System.err.println("actual(events)="+actualEventsCRC) ;
+
+		long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
+		System.err.println("actual(plans)="+actualPlansCRC) ;
+
+		// ---
+
+		final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
+
+		// ---
+
+		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,10.);
+		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,10.);
+		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 10.);
+		TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
+
+	}
+
+	@Test
+	@Ignore
 	public void testBlockage50() {
 
 		String [] args = {
