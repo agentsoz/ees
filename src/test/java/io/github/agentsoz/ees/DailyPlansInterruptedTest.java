c3742dddf4da6a1a2b84f86c61c1099aa2bc2fcc
Dhirendra Singh <dhi.singh@gmail.com>
2018-09-12 10:13:21 +1000

    Removed outdated tests

--- src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
+++ src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
@@ -42,52 +42,5 @@ public class DailyPlansInterruptedTest {
         TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
     }
 
-    @Test
-    @Ignore
-    public void testDailyPlansInterrupted() {
-
-        String[] args = {
-                "--config", "scenarios/surf-coast-shire/plan-interrupted/scenario_main.xml",
-                "--logfile", utils.getOutputDirectory()+"../scenario.log",
-                "--loglevel", "INFO",
-                "--seed", "12345",
-                "--safeline-output-file-pattern", utils.getOutputDirectory()+"../safeline.%d%.out",
-                "--matsim-output-directory", utils.getOutputDirectory(),
-                "--jillconfig", "--config={" +
-                "agents:[]," +
-                "logLevel: WARN," +
-                "logFile: \""+utils.getOutputDirectory()+"../jill.log\"," +
-                "programOutputFile: \""+utils.getOutputDirectory()+"../jill.out\"," +
-                "randomSeed: 12345" + // jill random seed
-                //",numThreads: 1"+ // run jill in single-threaded mode so logs are deterministic
-                "}",
-                "--x-congestion-config", "180:0.33:1.0",
-                "--x-load-bdi-agents-from-matsim-plans-file", "true", // Load agents from MATSim plans file
-                "--x-messages-file", "scenarios/surf-coast-shire/plan-interrupted/scenario_messages.json", // specifies when to send evac now msg
-                "--x-zones-file", "scenarios/surf-coast-shire/plan-interrupted/Anglesea_SA1s_WSG84.json", // map from zone (SA1) ids to shapes
-        };
-
-        Main.main(args);
-
-        final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
-        long actualEventsCRC = CRCChecksum.getCRCFromFile( actualEventsFilename ) ;
-        System.err.println("actual(events)="+actualEventsCRC) ;
-
-        long actualPlansCRC = CRCChecksum.getCRCFromFile( utils.getOutputDirectory() + "/output_plans.xml.gz" ) ;
-        System.err.println("actual(plans)="+actualPlansCRC) ;
-
-        // ---
-
-        final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
-
-        // ---
-
-        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,5.);
-        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,5.);
-        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 5.);
-        TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
-
-    }
-
 }
 
