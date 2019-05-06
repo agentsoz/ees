b635bbd967bf7696c4547b3b94056062de7af3ac
Dhirendra Singh <dhi.singh@gmail.com>
2019-05-06 10:58:20 +1000

    Util to convert synthetic popn CSV to MATSim XML

---
+++ src/main/java/io/github/agentsoz/ees/util/SynthpopToMatsim.java
@@ -0,0 +1,170 @@
+package io.github.agentsoz.ees.util;
+
+/*-
+ * #%L
+ * Emergency Evacuation Simulator
+ * %%
+ * Copyright (C) 2014 - 2019 by its authors. See AUTHORS file.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.*;
+import org.matsim.core.api.internal.MatsimWriter;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.GeotoolsTransformation;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+public class SynthpopToMatsim {
+
+    private static String inputCsvGzip = null;
+    private static String matsimPopulationFile = "plans.xml";
+    private static String matsimOutputCoordinateSystem = "EPSG:4326";
+
+    private static String usage() {
+        return "usage: "
+                + SynthpopToMatsim.class.getName()
+                + "  [options] \n"
+                + "   --incsv FILE   Synthetic population CSV to convert (.csv.gz from https://github.com/agentsoz/synthetic-population)\n"
+                + "   --outxml FILE  Output file in MATSim plans XML format (default is '"+matsimPopulationFile+"')\n"
+                + "   --wkt STRING   Coordinate reference system to use for generated output (default is '"+matsimOutputCoordinateSystem+"')\n"
+                ;
+    }
+
+    private static void parse(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--incsv":
+                    if (i + 1 < args.length) {
+                        i++;
+                        inputCsvGzip = Paths.get(args[i]).toAbsolutePath().normalize().toString();
+                    }
+                    break;
+                case "--outxml":
+                    if (i + 1 < args.length) {
+                        i++;
+                        matsimPopulationFile = Paths.get(args[i]).toAbsolutePath().normalize().toString();
+                    }
+                    break;
+                case "--wkt":
+                    if (i + 1 < args.length) {
+                        i++;
+                        matsimOutputCoordinateSystem = args[i];
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (inputCsvGzip == null) {
+            abort(null);
+        }
+    }
+
+    private static void abort(String err) {
+        if (err != null && !err.isEmpty()) {
+            System.err.println("\nERROR: " + err + "\n");
+        }
+        System.out.println(usage());
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        parse(args);
+
+        Iterable<CSVRecord> records = null;
+        CSVFormat format = CSVFormat.DEFAULT.withQuote('\"').withFirstRecordAsHeader();
+        try {
+            records = format.parse(new InputStreamReader(new GZIPInputStream(new FileInputStream(inputCsvGzip))));
+        } catch (IOException e) {
+            e.printStackTrace();
+            abort("Could not read input CSV file '"+ inputCsvGzip +"'");
+        }
+
+
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        CoordinateTransformation ct = new GeotoolsTransformation(
+                "EPSG:4326",
+                matsimOutputCoordinateSystem);
+
+        int personId = 0;
+        for (CSVRecord record : records) {
+            Map<String,String> map = record.toMap();
+            for (String key : map.keySet()) {
+                System.out.println(key + "=" + map.get(key));
+            }
+            scenario.getPopulation().addPerson(createPersonWithActivity(
+                    Integer.toString(personId++),
+                    "home",
+                    0,
+                    0,
+                    21600,
+                    scenario.getPopulation().getFactory()));
+            break;
+        }
+
+        // Finally, write this population to file
+        MatsimWriter popWriter = new PopulationWriter(scenario.getPopulation(), scenario.getNetwork());
+        popWriter.write(matsimPopulationFile);
+
+    }
+
+    public static Person createPersonWithActivity(
+            String id,
+            String actType,
+            double x,
+            double y,
+            double actEndTime,
+            PopulationFactory populationFactory) {
+
+        Coord matSimCoord = new Coord(x,y);
+
+        // Create a new plan
+        Plan plan = populationFactory.createPlan();
+
+        // Create a new activity with the end time and add it to the plan
+        Activity act = populationFactory.createActivityFromCoord(actType, matSimCoord);
+        act.setEndTime(actEndTime);
+        plan.addActivity(act);
+
+        // Add a new leg to the plan. Needed, otherwise MATSim won't add this
+        // leg-less plan to the simulation
+        plan.addLeg(populationFactory.createLeg("car"));
+
+        // Create a second activity (all plans must end in an activity)
+        act = populationFactory.createActivityFromCoord(actType.toString(), matSimCoord);
+        plan.addActivity(act);
+
+        Person person = populationFactory.createPerson(Id.createPersonId(id));
+        person.addPlan(plan);
+        return person;
+    }
+
+
+}
