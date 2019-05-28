7b40cae7a5f6e8b999b89073075e974129d75f05
Dhirendra Singh <dhi.singh@gmail.com>
2019-05-28 12:20:16 +1000

    Tests still unstable on tracvis with slack=0; Moving to slack=1

--- src/test/java/io/github/agentsoz/ees/SituationAwarenessTest.java
+++ src/test/java/io/github/agentsoz/ees/SituationAwarenessTest.java
@@ -57,9 +57,9 @@ public class SituationAwarenessTest {
 		// If the full events comparison fails (possibly due to multi-threading differences on travis/other),
 		// then use the checks below, adjusting slack as needed,
 		// but ideally keeping it below 10 secs; dhi 28/may/19
-		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,0.);
-		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,0.);
-		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 0.);
+		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 1.);
 	}
 
 }
