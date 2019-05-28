0eb983ecc6faed398b65cbba1a60b79f7945c5df
Dhirendra Singh <dhi.singh@gmail.com>
2019-05-28 14:41:00 +1000

    DailyPlansInterruptedTest: tightened slack

--- src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
+++ src/test/java/io/github/agentsoz/ees/DailyPlansInterruptedTest.java
@@ -56,10 +56,13 @@ public class DailyPlansInterruptedTest {
 
         final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
         final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
-        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,5.);
-        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,5.);
-        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 5.);
-        TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, false);
+        // TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, true);
+        // If the full events comparison fails (possibly due to multi-threading differences on travis/other),
+        // then use the checks below, adjusting slack as needed,
+        // but ideally keeping it below 10 secs; dhi 28/may/19
+        TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,1.);
+        TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,1.);
+        TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 1.);
     }
 
 }
