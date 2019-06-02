0ec14ed9c90a1946628a15cc05dec500c1351695
Dhirendra Singh <dhi.singh@gmail.com>
2019-06-03 08:26:58 +1000

    BlockageEquilTest: loosened slack (failing on travis)

--- src/test/java/io/github/agentsoz/ees/BlockageEquilTest.java
+++ src/test/java/io/github/agentsoz/ees/BlockageEquilTest.java
@@ -52,13 +52,13 @@ public class BlockageEquilTest {
 
 		final String actualEventsFilename = utils.getOutputDirectory() + "/output_events.xml.gz";
 		final String primaryExpectedEventsFilename = utils.getInputDirectory() + "/output_events.xml.gz";
-		TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, true);
+		//TestUtils.compareFullEvents(primaryExpectedEventsFilename,actualEventsFilename, true);
 		// If the full events comparison fails (possibly due to multi-threading differences on travis/other),
 		// then use the checks below, adjusting slack as needed,
 		// but ideally keeping it below 10 secs; dhi 28/may/19
-		//TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,1.);
-		//TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,1.);
-		//TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 1.);
+		TestUtils.comparingDepartures(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingArrivals(primaryExpectedEventsFilename,actualEventsFilename,1.);
+		TestUtils.comparingActivityStarts(primaryExpectedEventsFilename,actualEventsFilename, 1.);
 	}
 
 }
