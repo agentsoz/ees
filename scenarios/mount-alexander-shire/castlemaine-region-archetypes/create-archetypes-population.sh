09ee79f21d5020fe8942917c825b11c15edfe741
Dhirendra Singh <dhi.singh@gmail.com>
2019-06-04 09:25:58 +1000

    Updated script for creating archetypes population; towards #34

--- scenarios/mount-alexander-shire/castlemaine-region-archetypes/create-archetypes-population.sh
+++ scenarios/mount-alexander-shire/castlemaine-region-archetypes/create-archetypes-population.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-INCSV=./archetypes.csv.gz
-printf "Creating $INCSV\n"
+INCSV=./archetypes-population.csv.gz
+OUTXML=./archetypes-population.xml
+
 Rscript --vanilla './create_archetypes_csv.R'
 
 CMD="java -cp ../../../target/ees-2.1.1-SNAPSHOT.jar \
   io.github.agentsoz.ees.util.SynthpopToMatsim \
-  --incsv ./archetypes.csv.gz \
-  --outxml ./archetypes.xml
+  --incsv $INCSV \
+  --outxml $OUTXML \
   --incrs \"EPSG:4326\" \
   --outcrs \"EPSG:28355\" \
   "
+echo $CMD; eval $CMD
+
+CMD="gzip -f -9 $OUTXML"
 echo $CMD; eval $CMD
