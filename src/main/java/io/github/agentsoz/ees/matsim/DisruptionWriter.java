814a15167cb5bd108bb1c1443c014e1159717270
Dhirendra Singh <dhi.singh@gmail.com>
2020-06-19 13:05:02 +1000

    Updated license header

--- src/main/java/io/github/agentsoz/ees/matsim/DisruptionWriter.java
+++ src/main/java/io/github/agentsoz/ees/matsim/DisruptionWriter.java
@@ -2,9 +2,9 @@ package io.github.agentsoz.ees.matsim;
 
 /*-
  * #%L
- * BDI-ABM Integration Package
+ * Emergency Evacuation Simulator
  * %%
- * Copyright (C) 2014 - 2018 by its authors. See AUTHORS file.
+ * Copyright (C) 2014 - 2020 by its authors. See AUTHORS file.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- src/main/java/io/github/agentsoz/ees/matsim/MATSimEvacModel.java
+++ src/main/java/io/github/agentsoz/ees/matsim/MATSimEvacModel.java
@@ -2,20 +2,20 @@ package io.github.agentsoz.ees.matsim;
 
 /*
  * #%L
- * BDI-ABM Integration Package
+ * Emergency Evacuation Simulator
  * %%
- * Copyright (C) 2014 - 2015 by its authors. See AUTHORS file.
+ * Copyright (C) 2014 - 2020 by its authors. See AUTHORS file.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
