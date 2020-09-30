490bfa42f905cb2d060cccd0d206492ab55a99af
Dhirendra Singh <dhi.singh@gmail.com>
2020-09-30 20:39:51 +1000

    Towards ensemble runs of the Aireys scenario

--- ees/scenarios/surf-coast-shire/aireys/base/run.sh
+++ ees/scenarios/surf-coast-shire/aireys/base/run.sh
@@ -4,5 +4,5 @@
 
 eesdir=./../../../../target/eeslib-2.1.1-SNAPSHOT
 jars=${eesdir}/libs/*:${eesdir}/eeslib-2.1.1-SNAPSHOT.jar
-cmd="java -cp $jars io.github.agentsoz.ees.Run --config ees.xml"
+cmd="java -Xmx16g -Xms16g -cp $jars io.github.agentsoz.ees.Run --config ees.xml"
 echo $cmd && eval $cmd
