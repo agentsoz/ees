486a0f3b19c9e234111b04796b234ba170cbfc72
Dhirendra Singh <dhi.singh@gmail.com>
2020-10-09 12:59:06 +1100

    aireys messaaging scenario for comparison

---
+++ ees/scenarios/surf-coast-shire/aireys/base/run-msg1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Must be run from the directory containing this script
+
+eesdir=./../../../../target/eeslib-2.1.1-SNAPSHOT
+jars=${eesdir}/libs/*:${eesdir}/eeslib-2.1.1-SNAPSHOT.jar
+cmd="java -Xmx16g -Xms16g -cp $jars io.github.agentsoz.ees.Run --config ees-msg1.xml"
+echo $cmd && eval $cmd
