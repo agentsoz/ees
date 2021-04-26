5cc66ba34f0a12ae47a7bb41bee6e2f883c9f35d
Dhirendra Singh <dhi.singh@gmail.com>
2021-04-26 16:02:34 +1000

    Retrying SCS test on CI server (passes locally) #49

--- ees/src/test/java/io/github/agentsoz/ees/agents/archetype/SCSArchetypesBasicTest.java
+++ ees/src/test/java/io/github/agentsoz/ees/agents/archetype/SCSArchetypesBasicTest.java
@@ -25,7 +25,6 @@ package io.github.agentsoz.ees.agents.archetype;
 
 import io.github.agentsoz.ees.Run;
 import io.github.agentsoz.util.TestUtils;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.testcases.MatsimTestUtils;
@@ -44,7 +43,6 @@ public class SCSArchetypesBasicTest {
     public MatsimTestUtils utils = new MatsimTestUtils() ;
 
     @Test
-    @Ignore // FIXME: fails on GitHub CI server, 26/Apr/21 Dhi
     public void test() {
 
         utils.getOutputDirectory(); // creates a clean one so need to call this first
