5ad5757218c8b2f318fcfad95af44771501c2702
Dhirendra Singh <dhi.singh@gmail.com>
2022-03-29 12:20:45 +1100

    update headers

--- ees/src/main/java/io/github/agentsoz/ees/agents/archetype/CommunityGuided.java
+++ ees/src/main/java/io/github/agentsoz/ees/agents/archetype/CommunityGuided.java
@@ -4,7 +4,7 @@ package io.github.agentsoz.ees.agents.archetype;
  * #%L
  * Emergency Evacuation Simulator
  * %%
- * Copyright (C) 2014 - 2021 by its authors. See AUTHORS file.
+ * Copyright (C) 2014 - 2022 by its authors. See AUTHORS file.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- ees/src/test/java/io/github/agentsoz/ees/agents/archetype/it/CastlemaineRegionArchetypesBaselineIT.java
+++ ees/src/test/java/io/github/agentsoz/ees/agents/archetype/it/CastlemaineRegionArchetypesBaselineIT.java
@@ -5,7 +5,7 @@ package io.github.agentsoz.ees.agents.archetype.it;
  * #%L
  * Emergency Evacuation Simulator
  * %%
- * Copyright (C) 2014 - 2021 by its authors. See AUTHORS file.
+ * Copyright (C) 2014 - 2022 by its authors. See AUTHORS file.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
