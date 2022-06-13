00ab3c131b484aeb170c7a5d5714c992bdcfc457
Dhirendra Singh <dhi.singh@gmail.com>
2022-06-13 14:05:12 +1000

    add seqld baseline IT

---
+++ ees/src/test/java/io/github/agentsoz/ees/it/SouthEastQld0IT.java
@@ -0,0 +1,62 @@
+
+package io.github.agentsoz.ees.it;
+
+
+/*-
+ * #%L
+ * Emergency Evacuation Simulator
+ * %%
+ * Copyright (C) 2014 - 2022 by its authors. See AUTHORS file.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import io.github.agentsoz.ees.Run;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Dhi Singh
+ *
+ */
+
+
+public class SouthEastQld0IT {
+
+
+    // have tests in separate classes so that they run, at least under maven, in separate JVMs.  kai, nov'17
+
+    private static final Logger log = LoggerFactory.getLogger(SouthEastQld0IT.class) ;
+
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+    @Test
+    public void test() {
+
+        utils.getOutputDirectory(); // creates a clean one so need to call this first
+        String[] args = {
+                "--config", "scenarios/qld/south_east_qld/0.baseline.xml",
+        };
+        Run.main(args);
+
+    }
+
+}
+
